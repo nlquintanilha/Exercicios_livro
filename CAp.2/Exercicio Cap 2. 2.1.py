6e710d3b76ede3c5d8b2149168effe987fee94cf
Natalia Lima <nlqprof@gmail.com>
2025-07-09 14:26:28 -0300

    Ajustei indentação

--- CAp.2/Exercicio Cap 2. 2.1.py
+++ CAp.2/Exercicio Cap 2. 2.1.py
@@ -2,12 +2,8 @@
 
 #O volume de uma esfera com raio r é 4|3 pi r3 . Qual é o volume de uma esfera com raio 5?
 
- import math
+import math
 raio = 5
 
 volume = (4/3) * math.pi * (raio ** 3)
 print (volume)
-
-
-
-
